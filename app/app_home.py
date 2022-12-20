33eced20bbae6d9452f04f431a54fe362fa58fc7
shahp7575 <shahp7575@gmail.com>
2022-12-20 01:01:57 -0500

    app with script

--- app/app_home.py
+++ app/app_home.py
@@ -1,6 +1,10 @@
+import sys
 import streamlit as st
 from streamlit_lottie import st_lottie
 from lottie import lottie_book
+
+sys.path.insert(0, 'corny_ai/')
+from corny_ai import get_corny, header
 
 st.set_page_config(layout='centered', page_title='Corny AI')
 
@@ -9,14 +13,18 @@ st.markdown("<h1 style='text-align: center;'>Corny AI</h1>", unsafe_allow_html=T
 st.markdown("<h4 style='text-align: center;'>AI which is sometimes very funny</h4>", unsafe_allow_html=True)
 
 prompt = st.text_input(label="dad_joke_prompt", 
-                    placeholder="What was so badly broken in Breaking Bad?", 
+                    placeholder="Q: What did iphone say to android?", 
                     label_visibility="collapsed")
 
-temp = st.slider(label="temperature", 
+temp = st.slider(label="Temperature", 
                 min_value=0.0, 
                 max_value=1.0,
                 value=0.75,
-                label_visibility="collapsed")
+                label_visibility="visible")
 
-generate = st.button("Generate")
-
+if st.button("Generate"):
+    with st.spinner():
+        out = get_corny(prompt=header+prompt,
+                        temperature=temp,
+                        max_tokens=100)
+        st.markdown(out['choices'][0]['text'])
